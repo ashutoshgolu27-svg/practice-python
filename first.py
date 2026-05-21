5f32933ea8cb447d745eb6bbd8aa6f9c1f907d67
shubham kumar <ashutoshgolu27@gmail.com>
2026-05-21 20:41:17 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,948 @@
+#  # student=["shubham","raju","ashu","mohit","raja"]
+# # print(student)
+# # mystu=list(student)
+# # print(mystu)
+
+# # student=("raju","anuj","sanjeev","manju")
+# # print(student)
+# # print(len(student))
+
+# # student=tuple(("raju","sanju","anju","manoj"))
+# # print(student)
+# # print(type(student))
+
+# # student=("raju","sanju","anju","manoj")
+# # print(student[0])
+# # print(student[1])
+# # print(student[2])
+
+# # student=("raju","sanju","anju","manoj")
+# # print(student[-1])
+# # print(student[-2])
+
+# # student=("raju","sanju","anju","manoj","sohan","rohan")
+# # print(student[1:4])
+
+# # student=("raju","sanju","anju","manoj","rohan")
+# # print(student)
+# # st1=list(student)
+# # print(st1)
+# # st1[1]="anita"
+# # student=tuple(st1)
+# # print(student)
+
+# # student=("raju","sanju","anju","manoj","rohan")
+# # print(student)
+# # (st1, st2, st3, st4, st5)=student
+# # print(st1)
+# # print(st2)
+# # print(st4)
+
+
+# # student=("raju","sanju","anju","manoj","rohan","raman")
+# # print(student)
+# # (st1,st2,st3,*myst)=student
+# # print(st1)
+# # print(st2)
+# # print(myst)
+
+
+# # age=(34,25,4,25,34,54,45,54)
+# # print(age)
+# # ans=age.count(25)
+# # print(ans)
+
+
+# # age=(34,25,4,25,34,54,45,54)
+# # print(age)
+# # ans=age.index(4)
+# # print(ans)
+
+
+# # set
+
+# # city={"bhopal","satna","ujjain","maihar","itarsi"}
+# # print(len(city))
+
+# # city={"bhopal","ujjain","itarsi","satna"}
+# # name=set(("raju","sachin","shubham","pankaj"))
+# # print(type(name))
+# # print(name)
+
+# # loop on set
+
+# # city={"bhopal","ujjain","itarsi","satna"}
+# # for a in city:
+# #     print(a)
+
+
+# # city={"bhopal","ujjain","itarsi","satna"}
+# # print(city)
+# # city.add("pune")
+# # print(city)
+
+
+# # city={"bhopal","ujjain","itarsi","satna"}
+# # city1={"jabalpur","gwalior"}
+# # city.update(city1)
+# # print(city)
+
+# #delete any one item in set
+
+# # city={"bhopal","ujjain","itarsi","satna"}
+# # print(city)
+# # myval=city.pop()
+# # print(city)
+# # print(myval)
+
+
+# # x = {"apple","banana","cherry"}
+# # y = {"google","microsoft","apple"}
+# # x.intersection_update(y)
+# # print(x)
+
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bhopal",
+# # }
+# # print(type(student))
+# # print(student)
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bhopal",
+# # }
+# # print(len(student))
+# # print(student)
+
+# # student=dict(name="shubham",city="bihar",fee=30000)
+# # print(type(student))
+
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # print(student["rollno"])
+# # print(student["city"])
+
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # ans=student.keys()
+# # print(ans)
+
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # ans=student.values()
+# # print(ans)
+
+
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # ans=student.items()
+# # print(ans)
+
+# #chnage value....of dict
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # print(student)
+# # student["city"]="indore"
+# # print(student)
+
+
+
+# #update name in dict...
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # print(student)
+# # student.update({"name":"sanju"})
+# # print(student)
+
+
+
+# #adding items in dict....
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # print(student)
+# # student["subject"]="java script"
+# # print(student)
+
+# #remove item in dict...
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # print(student)
+# # student.pop("city")
+# # print(student)
+
+
+# #pop item in dictt...
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # print(student)
+# # student.popitem()
+# # print(student)
+
+# #loop dictionary 
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # for a in student:
+# #     print(a)
+
+
+# #value find k liye
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # for a in student:
+# #     print(student[a])
+
+# #value or key sath me find krne k liye dict.. me
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # for a,b in student.items():
+# #     print(a,b)
+
+# # copy method k liye
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fee":30000
+# # }
+# # stu1=student.copy()
+# # print(stu1)
+
+
+# # condition statment
+
+# # name=input("enter a name:")
+# # print("welcome",name)
+
+
+
+
+# # a=5
+# # if a==5:
+# #     print("this is 5 value")
+
+
+# #find value input
+
+# # a=int(input("enter any number:"))
+# # print(a*3)
+
+# #use of if annd else in voting
+
+# # age=int(input("enter your age"))
+# # if age>=18:
+# #     print("you can vote")
+# # else:
+# #     print("sorry you can not vote")
+
+
+# #greater than in 2 value if else
+
+# # a=int(input("enter value a:"))
+# # b=int(input("enter value b:"))
+# # if a>b:
+# #     print("a is greater:",a)
+# # else:
+# #     print("b is greater:",b)
+
+# # tax find in if elif else
+
+# # sal=int(input("enter salary:"))
+# # if sal>=75600:
+# #     print("25% tax")
+# # elif sal>=50000:
+# #     print("20% tax")
+# # elif sal>=20000:
+# #     print("10% tax")
+# # else:
+# #     print("no tax")
+    
+# #greater than in 3 value if elif else
+
+# # a=int(input("enter a :"))
+# # b=int(input("enter b :"))
+# # c=int(input("enter c :"))
+# # if a>b and a>c:
+# #     print("A is greater")
+# # elif b>a and b>c:
+# #     print("B is greater")
+# # else:
+# #     print("c is greater")
+
+
+# #nested if statement 
+
+# # name="shubham"
+# # city="bihar"
+# # age=20
+
+# # if city=="bihar":
+# #     if age>=18:
+# #         print("you can join our python classes")
+        
+# #     else:
+# #         print("you can join only html classes")
+# # else:
+# #     print("sorry you can not join any classes")
+
+
+# #print value of 10 in while loop
+
+# # i=1
+# # while i<=10:
+# #     print(i)
+# #     i=i+1
+
+# #print even no in 20 while loop
+
+# # a=2
+# # while a<=20:
+# #     print(a)
+# #     a=a+2
+
+# # program to print 20 to 50 odd no
+
+# # a=21
+# # while a<=50:
+# #     print(a)
+# #     a=a+2
+
+# #table program in any no 
+
+# # a=int(input("enter any number"))
+# # i=1
+# # while i<=10:
+# #     ans=a*i
+# #     print(a," *",i," =",ans)
+# #     i=i+1
+
+# #continue statment in loop
+
+# # i=0
+# # while i<=20:
+# #     i=i+1
+# #     if i==14:
+# #         continue
+# #     print(i)
+
+# #break statment in loop
+
+# # i=1
+# # while i<=10:
+# #     if i==9:
+# #         break
+# #     print(i)
+# #     i=i+1
+
+# #for loop 
+
+# # name=["raju","sanju","shubham","seema"]
+# # for x in name:
+# #     print("mr",x)
+
+# # find even no in for loop 
+
+# # num=[20,5,13,12,92,16,81,69,54]
+# # for a in num:
+# #     if a%2==0:
+# #         print(a)
+
+# #find odd no in for loop
+
+# # num=[20,5,13,12,92,16,81,69,54]
+# # for a in num:
+# #     if a%2!=0:
+# #         print(a)
+
+
+# # tuple in for loop
+
+# # subject=("php","science","oracle","java","asp")
+# # for x in subject:
+# #     print("my subject is :",x)
+
+# #set in for loop
+
+# # city={"bhopal","bihar","patna","ujjain"}
+# # for a in city:
+# #     print(a)
+
+# #dictionary in for loop
+
+# # student={
+# #     "rollno":121,
+# #     "name":"shubham",
+# #     "city":"bihar",
+# #     "fees":30000
+# # }
+# # for a in student:
+# #     print(a,student[a])
+
+# #range function in loop
+
+# # for a in range(6):
+# #     print(a)
+
+
+# #find 2 no parameter  of range function 
+
+# # for a in range (3,8):
+# #     print (a)
+ 
+# #find 3 no of parameter range
+
+# # for a in range (5,55,3):
+# #     print (a)
+
+# #find even no of parameter range 
+
+# # for a in range (2,41,2):
+# #     print (a)
+
+# #print odd no in range parameter
+
+# # for a in range (31,50,2):
+# #     print(a)
+
+#      # else in range  for loop 
+
+# # for a in range (2,31,2):
+# #     print(a)
+# # else:
+# #     print("loop is finished")
+
+# #nested loop in range for loop
+
+# # name=["sachin","mohan","anuj","sanjay"]
+# # sub=["php","java","oracle","asp"]
+# # for a in name:
+# #     for b in sub:
+# #         print (a,b)
+
+# #def function 
+
+# # def mycollege():        #function definition 
+# #     print("mycollege name is shubham")
+# # mycollege()              # fuction calling
+
+
+#  # multiple fun call      
+
+# # def mycollege():
+# #     print("welcome to cybrom")
+# # print("we learn python")
+# # mycollege()
+# # mycollege()
+# # mycollege()
+
+
+# # return value in funtion
+
+# # def myname():
+# #     return "i am shubham"
+# # print("welcome to bhopal")
+# # ans=myname()
+# # print(ans)
+
+# #adding multiple value in functions
+
+# # def myadd():
+# #     a=20
+# #     b=30
+# #     c=12
+# #     ans=a+b+c
+# #     return ans
+# # print("myadd")
+# # myans=myadd()
+# # print(myans)
+
+
+# #func arguments 
+
+# # def myname(fnm,snm):
+# #     print("my name is :",fnm,snm)
+# # print("welcome to bhopal")
+# # myname("shubham","kumar")
+
+# #calculation btn 2 parameters
+
+# # def myadd(a,b):
+# #     ans=a+b
+# #     print("addition",ans)
+
+# # print("my addition") 
+# # myadd(22,35)
+# # myadd(12,90)
+# # myadd(67,87)   
+
+# #fun in return and parameters
+
+# # def mycube(a):
+# #     ans=a*a*a
+# #     return ans
+# # print("welcome to bihar")
+# # myans=mycube(6)
+# # print("mycube:",myans)
+
+
+# #passing different data types
+
+# # def myname(nm):
+# #     print(nm[1])
+# #     print(nm[3])
+
+# # print("welcome")  
+# # name=["raja","sanju","manju","ranju"]  
+# # myname(name)
+
+
+# #arbitary arguments 
+
+# # def mydata(*nm):
+# #     print(type(nm))
+# #     print(nm[ 1])
+
+# # mydata("anju","manju","ranju")    
+
+# #add in arbitray arg..
+
+# # def myadd(*num):
+# #     sum=0
+# #     for i in num:
+# #         sum=sum+i
+# #     print("total :",sum) 
+
+# # print("addition program")
+# # myadd(10,29,34,100,350)     
+
+# #find maximum value in arbitry 
+
+# # def myadd(*num):
+# #      max=0
+# #      for i in num:
+# #          if i>max:
+# #              max=i
+# #      print("maximum no",max)
+     
+# # print("maximum program")
+# # myadd(10,20,700,800,900)  
+
+
+# #            
+# # a=10   #global
+# # def myfun():
+# # #     a=10  local
+# #     print(a)
+
+# # myfun()
+# # print(a)
+
+
+# # global local 
+
+# # a=20
+# # print(a)
+# # def myfun():
+# #     global a
+# #     a=10
+# #     print(a)
+
+# # myfun()
+# # print(a)    
+    
+# #lambda fun 
+
+ # x=lambda a:a*2
+# # print(x(5))
+
+
+# myadd=lambda a,b : a+b
+# ans=myadd(40,30)
+# print(ans)
+
+# mymulti=lambda a,b,c : a*b*c
+# print(mymulti(3,5,4))
+
+
+#python program to find largest three no using elif statment
+
+# a= int(input("enter no a " ))
+# b= int(input("enter no b " ))
+# c= int(input("enter no c " ))
+# if a>b and a>c:
+#     print("a is greater", a)
+# elif b>a and b>c:
+#     print("b is greater", b)
+# elif c>a and c>b:
+#     print("c is greater", c)
+# else:
+#     print("any two or three number are equal")
+
+
+
+# a=int(input("enter any number"))
+# if a>0:
+#     print("This number is positive", a)
+# elif a<0:
+#     print("This number is negative",a)
+# else:
+#     print("This number is zero '0' ")
+
+# find even no in range
+
+# a=int(input("enter any digit : "))
+# sum=0
+# for i in range(1, a+1):
+#     if i%2==0:
+#         sum=sum+1
+# print("sum of even no form",1, "to", a, "is:",sum)        
+
+#pyhton program to check number is divisible by 5 and 11
+
+
+# Take input from user
+# num = int(input("Enter a number: "))
+
+# # Check divisibility
+# if num % 5 == 0 and num % 11 == 0:
+#     print("The number is divisible by both 5 and 11")
+# else:
+#     print("The number is NOT divisible by both 5 and 11")
+
+
+# Program to check character type
+
+# ch = input("Enter a character: ")
+
+# if ch.isalpha():
+#     print("It is an Alphabet")
+# elif ch.isdigit():
+#     print("It is a Digit")
+# else:
+#     print("It is a Special Character")
+
+
+# Input from user uppercase or lowercase
+
+# ch = input("Enter a character: ")
+
+# # Check if it is uppercase or lowercase
+# if ch.isupper():
+#     print("Uppercase letter")
+# elif ch.islower():
+#     print("Lowercase letter")
+# else:
+#     print("Not an alphabet character")
+
+#class and object 
+
+# class mydata:
+#     name="sachin"
+#     age=42
+
+# ob1= mydata() 
+
+# print("name : ", ob1.name)
+# print("age ; ", ob1.age)
+
+
+# x=10
+# print(x<<2)
+
+
+# indexing 
+
+# s="shubham"
+# print(s.index("h"))
+# print(s.index("h",s.index("h")+1))
+# print(s.index("h",2,6))
+
+
+# s="programming"
+# print(s.index("a",2,-5))
+
+#slice 
+
+# s="python program"
+# print (s[:])
+
+# s="python program"
+# print(s[::-1])
+
+# s="python program"
+# print(s[5:-2])
+
+
+# s="python program"
+# print(s[20:100])
+
+# s="python program"
+# print(s[12:-13:-2])
+
+# s="python program"
+# print(s[12:-13:-100])
+
+# s="python program"
+# print(s[10:-6:-1])
+
+# s="python program"
+# print(s[6:-10:-3])
+
+# l=[10,20,30,40,50]
+# print(l[2:-3])
+
+# s="python program"
+# print(s[::-1] [::-1] [2:10] [3:5:-1] [2:2])
+
+#strings
+
+# print('this is "python"')
+# print('this is 'python'')
+
+
+# print("this is  \"c\"")
+# print('this is \'c++\'')
+
+#tuple
+
+# t=(1,2,3,4,5,"python","java")
+# print(t)
+# print(type(t))
+# print(id(t))
+# print(len(t))
+
+# t1=(1,2,3,4,5)
+# print(max(t1))
+# print(sum(t1))
+# print(min(t1))
+
+# t2=("python","java","php")
+# print(max(t2))
+# print(min(t2))
+# print(sum(t2)) #not supported
+
+
+# method of tuple.......
+
+
+# t=(1,2,3,4,5,"python","java")
+# print(t.count(2)) #frequency of any element
+# print(t.index("python")) #findout index position of any element
+
+
+
+#dictionary 
+
+# d={"name":"meera","age":34,"quali":"mca"}
+# print(d)
+# print(type(d))
+# print(id(d))
+# print(max(d))
+# print(min(d))
+# print(len(d))
+# print(sum(d)) #not supported
+
+#dictionary  
+
+
+# d={ 'name':'python','language':"backend",'city':''}
+# d.clear()
+# print(d)
+ 
+# d1= d.copy()
+# print(d,d1)
+# print(id(d),id(d1)) 
+
+
+
+#set
+# s= {1,2,3,'python','java','php'}
+# s.add(100)
+# print(s)
+
+# s.update('python')
+# print(s)
+
+
+# s.remove('php')
+# print(s)
+
+# s.discard(100)
+# print(s)
+
+# s= {1,2,3,'python','java','php'}
+# s1= s.copy()
+# print(s,s1)
+# print(id(s),id(s1))
+
+# s.clear()
+# print(s)
+
+
+#list 
+
+# l=[10,29,30,40,'python',]
+# l.append(100)
+# print(l)
+
+# l=[10,29,30,40,'python',]
+# # l.extend(100)
+# l.extend('java')
+# print(l)
+
+# l=[10,29,30,40,'python',]
+# l.insert(1,"php")
+# print(l)
+
+# l1=[10,29,30,40,'python',]
+# l2=l1.copy()
+# # print(l1,l2,sep=',')
+# print(id(l1),id(l2),sep=',')
+
+# l=[1,2,3,4,5,'python']
+# l.pop()
+# print(l)
+
+# l=[1,2,3,4,5,'python']
+# l.remove()
+# l.remove(5)
+# print(l)
+
+# l=[1,2,3,4,5,'python']
+# l.clear()
+# print(l)
+
+# l=[1,2,3,4,5,'python']
+# pos=l.index(5)
+# print(pos)
+
+# l = [1,2,3,4,5,5,5,'python']
+# pos = l.count(5)
+# print(pos)
+
+# l=[1,2,3,4,5,34,87,98]
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+
+
+
+
+# s1={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# s1.difference_update (s2)
+# print(s1)
+
+# s1={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# s1.intersection_update(s2)
+# print(s1)
+# print(s2)
+
+# s1={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# s1.symmetric_difference_update(s2)
+# print(s1)
+# print(s2)
+
+# s1={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# s1.isdisjoint(s2)
+# print(s1)
+# print(s2)
+
+# s1={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# print(s1.isdisjoint(s2))
+
+# s1={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# print(s1.issuperset(s2))
+
+# s1={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# print(s2.issuperset(s1))
+
+# s1={1,2,3,4,5,6,7,8,9,10}
+# s2={4,5,6,7}
+# print(s1.issubset(s2))
+
+# s1={1,2,3,4,5,6,7,8,9,10}
+# s2={4,5,6,7}
+# print(s2.issubset(s1))
+
+#frozenset
+
+# s={10,20,'python','java',20,10,30}
+# fs=frozenset(s)
+# print(fs)
+# print(type(fs))
+# print(id(fs))
+# print(len(fs))
+# print(max(fs))
+# print(min(fs))
+# print(sum(fs))
---
+++ practice.py
@@ -0,0 +1,65 @@
+# char_count = {}
+# for ch in char_count:
+#     if ch in char_count:
+#         char_count[ch] += 1
+#     else:
+#         char_count[ch] = 1
+# print(char_count)
+
+#find prime numberor not prime numbr
+
+# num = int(input("Enter a number: "))
+# if num <= 1:
+#     print("Not a Prime Number")
+# else:
+#     for i in range(2, num):
+#         if num % i == 0:
+#             print("Not a Prime Number")
+#             break
+#     else:
+#         print("Prime Number")
+
+
+
+# Q3.Count and print how many times 'football' appears in list.
+
+
+# Sports = ['cricket', 'football', 'tennis', 'football', 'hockey']
+# count = 0
+# for game in Sports:
+#     if game == 'football':
+#         count += 1
+# print("Football appears", count, "times")
+
+
+# Q4. Find and print the largest and smallest number in a list.
+# [8, 2, 15, 1, 9] without using max() and min().
+
+
+# numbers = [8, 2, 15, 1, 9]
+# largest = numbers[0]
+# smallest = numbers[0]
+# for num in numbers:
+#     if num > largest:
+#         largest = num
+#     if num < smallest:
+#         smallest = num
+# print("Largest number:", largest)
+# print("Smallest number:", smallest)
+
+
+# Q5. Write a code to print the key of a minimum value from the
+# following dictionary.
+
+# Sample :
+# Input:{‘math’:89,’phy’:80,’chem’:67,’eng’:75}
+# Output:chem
+
+marks = {'math': 89, 'phy': 80, 'chem': 67, 'eng': 75}
+min_key = None
+min_value = None
+for key, value in marks.items():
+    if min_value is None or value < min_value:
+        min_value = value
+        min_key = key
+print(min_key)
