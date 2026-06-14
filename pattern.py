61a41521227c49ed38c0becbe20e2d2f734d1753
shubham kumar <ashutoshgolu27@gmail.com>
2026-06-14 12:18:10 +0530

    traingle pattern

--- pattern.py
+++ pattern.py
@@ -1,6 +1,15 @@
+# n = 5
+
+# for i in range(n):
+#     for j in range(n):
+#         print("*", end=" ")
+#     print()
+
+
+
 n = 5
 
-for i in range(n):
-    for j in range(n):
+for i in range(1, n + 1):
+    for j in range(i):
         print("*", end=" ")
     print()
