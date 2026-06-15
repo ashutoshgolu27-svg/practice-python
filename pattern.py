8de82edd54f0208e0efb923956cdd689ec306382
shubham kumar <ashutoshgolu27@gmail.com>
2026-06-15 23:06:47 +0530

    pyramid

--- pattern.py
+++ pattern.py
@@ -21,9 +21,17 @@
 #         print(j, end=" ")
 #     print()
 
+# n = 5
+
+# for i in range(n, 0, -1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+
+
 n = 5
 
-for i in range(n, 0, -1):
-    for j in range(i):
-        print("*", end=" ")
-    print()
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    print("* " * i)
