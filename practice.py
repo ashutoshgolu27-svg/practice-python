54eb94a78c0af694d054dd225f095ff9f8fe2dcf
shubham kumar <ashutoshgolu27@gmail.com>
2026-06-23 16:32:39 +0530

    class object and variable

--- practice.py
+++ practice.py
@@ -647,12 +647,23 @@
 #object attribute delete
 
 
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+
+# s1 = Student("Shubham")
+
+# print(s1.name)
+
+# del s1.name
+
+#class variable and object
+
 class Student:
-    def __init__(self, name):
-        self.name = name
+    school = "ABC School"
 
-s1 = Student("Shubham")
+s1 = Student()
+s2 = Student()
 
-print(s1.name)
-
-del s1.name
+print(s1.school)
+print(s2.school)
