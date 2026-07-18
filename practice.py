380b94b1c880f46884082a00b21a157b9e29b468
shubham kumar <ashutoshgolu27@gmail.com>
2026-07-18 14:32:44 +0530

    numpy mean

--- practice.py
+++ practice.py
@@ -1634,8 +1634,15 @@
 # print(result)
 
 #numpy reshape
+# import numpy as np
+
+# arr = np.arange(1,13)
+
+# print(arr.reshape(3,4))
+
+#numpy mean
 import numpy as np
 
-arr = np.arange(1,13)
+arr = np.array([10,20,30,40])
 
-print(arr.reshape(3,4))
+print(np.mean(arr))
